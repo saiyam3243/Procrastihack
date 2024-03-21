0ee3f1da30f6d7f0d2867f3ae6eb1f496fb0c151
akshat <akshat.tandon@tum.de>
2024-03-21 18:52:44 +0100

    adding init.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Welt!</p>"
+
+# get_persona_mapping() -> Persona Mapping
+# TODO get_persona() -> Persona
+# get_top5_indicators(persona : Persona, personaMapping: dict) -> Top 5 metrics 
+# get_top3_companies(indicators: List[Indicator]) -> Top 3 companies
+# get_bottom3_companies(indicators: List[Indicator]) -> Bott 3 companies
+# get_top10_funds(indicators: List[Indicator]) -> Top 10 funds
+# get_persona_mappings() -> Persona Mappings
+
+@app.route("/api/persona")
+def get_persona_mapping():
+    pass
