c16d6d7e0bebeaba94fe1417addaf962ea15448d
Saiyam Jain <ge63req@mytum.com>
2024-03-26 23:17:44 +0100

    Add drawer stories

--- app/invest/page.tsx
+++ app/invest/page.tsx
@@ -1,8 +1,16 @@
 'use client'
-import { Card, CardContent, CardTitle } from '@/components/ui/card';
-import React from 'react';
+import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
+import React, { useEffect, useState } from 'react';
+interface Fund {
+    [key: string]: number;
+}
 
+interface FundsData {
+    top_k_funds: Fund;
+}
 const InvestPage: React.FC = () => {
+    const [persona, setPersona] = useState<any>(null);
+    const [funds, setFunds] = useState<FundsData[]>([]);
 
     const getPersona = async () => {
         const url = '/api/persona';
@@ -13,113 +21,89 @@ const InvestPage: React.FC = () => {
                     'Content-Type': 'application/json',
                 },
             });
-            const data = await response.json;
+            const data = await response;
             console.log(data);
-            alert('Status fetched successfully!');
         } catch (error) {
             console.error('Error:', error);
             alert('Data deletion failed. Check the console for details.');
         }
     }
 
+    const getFunds = async () => {
+        const url = '/api/get_top_k_funds';
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+            setFunds(data.top_k_funds);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Data deletion failed. Check the console for details.');
+        }
+    }
+    useEffect(() => {
+        getPersona();
+        getFunds();
+    }, []);
     return (
-        <div className="p-20 flex flex-col items-center justify-center space-y-20">
-            <div className=" text-extrabold text-5xl text-[#de3919]1">ESG</div>
-            <Card className="p-4 flex flex-col items-center justify-center">
+        <div className="flex min-h-screen flex-col items-center justify-center space-y-20 pt-24 pb-24 pl-12 pr-12">
+            <div className=" font-bold text-6xl text-[#de3919]">VisionVest</div>
+
+            <Card className="p-4 flex flex-col items-center justify-center shadow-2xl">
                 <CardTitle className="p-4">Persona : Climate change</CardTitle>
                 <CardContent className="text-center">
                     Focused on the impacts of investments on climate change, including greenhouse gas emissions, fossil fuels, and energy efficiency.
                 </CardContent>
             </Card>
-            <Card className="p-4 flex flex-col items-center justify-center w-4/5">
+            <Card className="p-4 flex flex-col items-center justify-center w-4/5 shadow-2xl">
                 <CardTitle className="p-4 font-bold mb-10">COMPANIES</CardTitle>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            49
-                        </div>
+                <CardContent className="text-center flex items-center justify-center space-x-10 text-center">
+                    <div>
+                        <div className="font-semibold">Adidas</div>
+
                         <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
                             88
                         </div>
+                    </div>
+                    <div>
+                        <div className="font-semibold">Apple</div>
+
                         <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            81
+                            93
                         </div>
                     </div>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
+                    <div>
+                        <div className="font-semibold">Siemens</div>
+
                         <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            89
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            64
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            34
+                            74
                         </div>
                     </div>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            21
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            91
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            77
-                        </div>
-                    </div>
-                </div>
+                </CardContent>
+                <CardFooter className="text-center font-semibold">
+                    ESG Scores
+                </CardFooter>
             </Card>
-            <Card className="p-4 flex flex-col items-center justify-center w-4/5">
-                <CardTitle className="p-4 font-bold mb-10">COMPANIES</CardTitle>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            49
+            <Card className="p-4 flex flex-col items-center justify-center w-4/5 shadow-2xl">
+                <CardTitle className="p-4 font-bold mb-10">FUNDS</CardTitle>
+                <CardContent className="text-center flex items-center justify-center space-x-10 text-center">
+                    {Object.entries(funds).map(([fundName, value]) => (
+                        <div>
+                            <div className="font-semibold">{(fundName).split(" ")[0]}</div>
+
+                            <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
+                                {Number(value) * 100}
+                            </div>
                         </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            88
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            81
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            89
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            64
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            34
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <div className="font-semibold"> Company name</div>
-                    <div className="flex m-10 space-x-10">
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            21
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            91
-                        </div>
-                        <div className="rounded-full bg-[#de3919] h-20 w-20 text-center flex items-center justify-center">
-                            77
-                        </div>
-                    </div>
-                </div>
+                    ))}
+                </CardContent>
+                <CardFooter className="text-center font-semibold">
+                    ESG Scores
+                </CardFooter>
             </Card>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,23 @@
 'use client'
-import Image from 'next/image'
-import Link from 'next/link'
 import { ParallaxProvider, useParallax, Parallax } from 'react-scroll-parallax';
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
-import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from '@/components/ui/drawer';
-import { data } from 'autoprefixer';
-import { MinusIcon, PlusIcon } from 'lucide-react';
+// import Stories from '@/components/home/stories';
+import { Quiz } from '@/components/home/quiz';
 export default function Home() {
   const router = useRouter();
-  const apiCall = async () => {
-    const url = '/api/python';
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response;
-      console.log(data);
-      alert('Status fetched successfully!');
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Data deletion failed. Check the console for details.');
-    }
-  }
-
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {/* <button className={`mb-3 text-2xl font-semibold`} onClick={apiCall}> Hello World</button> */}
-        <div className=" text-extrabold text-5xl text-[#de3919]1">ESG</div>
-        <div className="h-1/4">
-          <Parallax speed={-20} scale={[0.5, 0.9]}>
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-24 pl-12 pr-12">
+      <div className=" font-bold text-6xl text-[#de3919]">VisionVest</div>
+      <div>
+          <Parallax speed={20} scale={[0.8, 1.2]}>
             <video autoPlay loop muted className="w-full h-full object-cover" >
               <source src="/esg.mp4" type="video/mp4" />
             </video>
           </Parallax>
         </div>
-        <div className="h-1/4">
-          <Parallax speed={-20} scale={[0.5, 0.9]}>
-            <video autoPlay loop muted className="w-full h-full object-cover" >
-              <source src="/esg.mp4" type="video/mp4" />
-            </video>
-          </Parallax>
-        </div>
-        <div className="h-1/4">
-          <Parallax speed={-20} scale={[0.5, 0.9]}>
-            <video autoPlay loop muted className="w-full h-full object-cover" >
-              <source src="/esg.mp4" type="video/mp4" />
-            </video>
-          </Parallax>
-        </div>
-        <Drawer>
-          <DrawerTrigger asChild>
-            <Button>Open Drawer</Button>
-          </DrawerTrigger>
-          <DrawerContent>
-            <div className="mx-auto w-2/3 max-w-sm">
-              <DrawerHeader>
-                <DrawerTitle>Move Goal</DrawerTitle>
-                <DrawerDescription>Set your daily activity goal.</DrawerDescription>
-              </DrawerHeader>
-              <div className="flex">
-                <Image src="/boy.png" alt="ESG Image" width={100} height={100} />
-                <div className="m">
-                  <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-                    <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                      <span className="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
-                    </div>
-                    <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
-                  </div>
-                </div>
-              </div>
-              <DrawerFooter>
-                <Button>Submit</Button>
-                <DrawerClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DrawerClose>
-              </DrawerFooter>
-            </div>
-          </DrawerContent>
-        </Drawer>
-        <Button onClick={() => router.push('/invest')}>Start Investing</Button>
-      </main>
+        <Quiz />
+      <Button onClick={() => router.push('/invest')}>Lets Invest</Button>
+    </main>
   )
 }
