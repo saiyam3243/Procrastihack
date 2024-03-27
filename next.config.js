4a0a6f04f0c8d2270e8bcd44788e076ca72864d1
Saiyam Jain <ge63req@mytum.com>
2024-03-27 12:07:21 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,10 +7,18 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/api/:path*'
-            : '/api/',
+            : 'https://visionvest.vercel.app/api/',
       },
     ]
   },
+  experimental: {
+		outputFileTracingExcludes: {
+			'/api/': [
+				'./node_modules/@next/swc-linux-x64-musl/**/*',
+				'./node_modules/@next/swc-linux-x64-gnu/**/*',
+			],
+		},
+	},
 }
 
 module.exports = nextConfig
