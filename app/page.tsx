bed5d2e45c26e725e691052d61de5e01211e4940
akshat <akshat.tandon@tum.de>
2024-04-05 16:49:58 +0200

    improved homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,19 @@ import { useRouter } from 'next/navigation'
 import { Quiz } from '@/components/home/quiz';
 import { VestGPT } from '@/components/home/vestgpt';
 import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
+
 export default function Home() {
   const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-24 pl-12 pr-12">
-      <div className=" font-bold text-6xl text-[#de3919]">VisionVest</div>
-      <div>
-          <video autoPlay loop muted className="w-full h-full object-cover" >
+      <div className=" font-bold text-6xl text-[#de3919] mb-10 z-10">VisionVest</div>
+      <div className='absolute inset-0 z-0'>
+          <video autoPlay loop muted className="w-screen h-screen object-cover" >
             <source src="/esg.mp4" type="video/mp4" />
           </video>
       </div>
-      <div className="fixed bottom-8 right-8 font-bold text-white">
-        <VestGPT />
-      </div>
 
-      <Card className="p-4 flex flex-col items-center justify-center shadow-2xl w-3/4 mb-10">
+      <Card className="p-4 flex flex-col items-center justify-center shadow-2xl w-3/4 mb-10 z-10">
         <CardTitle className="p-4">Let's begin with investment!</CardTitle>
         <CardContent className="text-center w-2/3">
           Don't know about ESG or how to start with investments? No worries! Go through the conversation and quizzes below to strengthen your knowledge      
@@ -29,7 +27,7 @@ export default function Home() {
         </CardFooter>
       </Card>
 
-      <Card className="p-4 flex flex-col items-center justify-center shadow-2xl  w-3/4 mb-10">
+      <Card className="p-4 flex flex-col items-center justify-center shadow-2xl  w-3/4 mb-10 z-10">
         <CardTitle className="p-4">Let's begin with investment!</CardTitle>
         <CardContent className="text-center w-2/3">
           If you have already have some knowledge about investing, you can skip the quiz and invest directly. For questions about investing, you can always ask VestGPT.
@@ -38,6 +36,10 @@ export default function Home() {
           <Button className="w-40 mt-4" onClick={() => router.push('/invest')}> Invest</Button>
         </CardFooter>
       </Card>
+
+      <div className="fixed bottom-8 right-8 font-bold text-white z-10">
+        <VestGPT />
+      </div>
     </main>
   )
 }
